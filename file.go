36d212f9f9261b8a423fc81fe5bfc8333a23fc28
fangkun0422 <fangkun0422@163.com>
2020-12-29 21:35:28 +0800

    整理代码

--- file.go
+++ file.go
@@ -8,20 +8,6 @@ import (
 	"os"
 )
 
-func mvfile(s, d string) {
-	r, err := ioutil.ReadDir(s)
-	if err != nil {
-		log.Panicf("移动文件时，读取目录失败%v", err)
-	}
-
-	for _, fileName := range r {
-		if fileName.Name() == ".git" {
-			continue
-		}
-		os.Rename(s+"/"+fileName.Name(), d+"/"+fileName.Name())
-	}
-}
-
 func delfile(file string) {
 	r, err := ioutil.ReadDir(file)
 	if err != nil {
